10f83d74880d946b6b8d2d734c8dfcf854ce6e50
cwb <cwb@163.com>
2018-08-26 18:48:32 +0800

    提交代码

--- springcloudServer/src/main/java/com/mycloud/server/CloudServer.java
+++ springcloudServer/src/main/java/com/mycloud/server/CloudServer.java
@@ -2,7 +2,7 @@ package com.mycloud.server;
 
 public class CloudServer {
     public static void main(String[] args) {
-        System.out.println("im cloud server005");
-        System.out.println("im cloud server004");
+        System.out.println("im cloud server006");
+        System.out.println("im cloud server007");
     }
 }
