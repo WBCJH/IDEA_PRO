58b81ef2c4b2f84cec8160f91cfc85398e38e0ef
cwb <cwb@163.com>
2018-08-26 16:19:14 +0800

    修改了

--- springcloudclient/src/main/java/client/CloudClient.java
+++ springcloudclient/src/main/java/client/CloudClient.java
@@ -2,6 +2,6 @@ package client;
 
 public class CloudClient {
     public static void main(String[] args) {
-        System.out.println("Im cloudClient");
+        System.out.println("Im cloudClient222");
     }
 }
