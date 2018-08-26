c85e1ac83f95575cf9b1fb4542bcdba92408595a
WBCJH <wj271363216@163.com>
2018-08-26 20:25:22 +0800

    Update Branch004.java

--- springcloudclient/src/main/java/branch004/Branch004.java
+++ springcloudclient/src/main/java/branch004/Branch004.java
@@ -2,6 +2,6 @@ package branch004;
 
 public class Branch004 {
     public static void main(String[] args) {
-        System.out.println("branch 004");
+        System.out.println("branch 004-1");
     }
 }
