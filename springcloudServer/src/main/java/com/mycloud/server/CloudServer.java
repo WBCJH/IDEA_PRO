c43a0d459ca2c7c1a14b687d0df5f2169c6d3cb6
cwb <cwb@163.com>
2018-08-26 17:32:01 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	springcloudServer/src/main/java/com/mycloud/server/CloudServer.java

--- springcloudServer/src/main/java/com/mycloud/server/CloudServer.java
+++ springcloudServer/src/main/java/com/mycloud/server/CloudServer.java
@@ -2,7 +2,6 @@ package com.mycloud.server;
 
 public class CloudServer {
     public static void main(String[] args) {
-        System.out.println("Im cloudServer222");
-        System.out.println("Im cloudServer555");
+        System.out.println("Im cloudServer123");
     }
 }
