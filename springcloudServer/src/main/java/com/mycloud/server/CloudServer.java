c8f056f623f2d4d37126863073f59c53c17fcfda
WBCJH <wj271363216@163.com>
2018-08-26 18:11:18 +0800

    Update CloudServer.java

--- springcloudServer/src/main/java/com/mycloud/server/CloudServer.java
+++ springcloudServer/src/main/java/com/mycloud/server/CloudServer.java
@@ -2,7 +2,7 @@ package com.mycloud.server;
 
 public class CloudServer {
     public static void main(String[] args) {
-        System.out.println("im cloud server111222444");
-        System.out.println("im cloud server111222333");
+        System.out.println("im cloud server111222444555");
+        System.out.println("im cloud server111222333555");
     }
 }
