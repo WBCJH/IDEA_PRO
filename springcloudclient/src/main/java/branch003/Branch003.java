2bcd1ecd5dc82976af8c1ba6aa4401d9b2a9dc63
cwb <cwb@163.com>
2018-08-26 19:58:57 +0800

    提交代码

--- springcloudclient/src/main/java/branch003/Branch003.java
+++ springcloudclient/src/main/java/branch003/Branch003.java
@@ -2,6 +2,6 @@ package branch003;
 
 public class Branch003 {
     public static void main(String[] args) {
-        System.out.println("im hello branch003");
+        System.out.println("im hello branch0034");
     }
 }
