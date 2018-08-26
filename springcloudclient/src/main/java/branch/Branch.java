7ac5cbec29f492e1e54a7e852b87d0b957e39ecb
cwb <cwb@163.com>
2018-08-26 19:24:52 +0800

    提交代码

---
+++ springcloudclient/src/main/java/branch/Branch.java
@@ -0,0 +1,7 @@
+package branch;
+
+public class Branch {
+    public static void main(String[] args) {
+        System.out.println("hello everyone");
+    }
+}
--- springcloudclient/src/main/java/client/Test.java
+++ springcloudclient/src/main/java/client/Test.java
@@ -2,6 +2,6 @@ package client;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello everyone");
+        System.out.println("hello everyoneTest");
     }
 }
