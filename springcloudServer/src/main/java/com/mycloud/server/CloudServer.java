4a7dcbbf57f58cb779d5d9c85c5594d895ad5b8e
cwb <cwb@163.com>
2018-08-26 15:58:01 +0800

    提交项目

---
+++ springcloudServer/src/main/java/com/mycloud/server/CloudServer.java
@@ -0,0 +1,7 @@
+package com.mycloud.server;
+
+public class CloudServer {
+    public static void main(String[] args) {
+        System.out.println("Im cloudServer");
+    }
+}
---
+++ springcloudclient/src/main/java/client/CloudClient.java
@@ -0,0 +1,7 @@
+package client;
+
+public class CloudClient {
+    public static void main(String[] args) {
+        System.out.println("Im cloudClient");
+    }
+}
