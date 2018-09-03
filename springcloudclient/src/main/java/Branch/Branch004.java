e107e9f4554702a7139f181759526c77f589caf4
xuchangcheng <xuchangcheng@163.com>
2018-09-03 09:26:14 +0800

    提交

---
+++ springcloudclient/src/main/java/Branch/Branch004.java
@@ -0,0 +1,12 @@
+package Branch;
+
+/**
+ * @author chenwenbing
+ * @description
+ * @date 2018/9/3 上午9:25
+ */
+public class Branch004 {
+    public static void main(String[] args) {
+        System.out.println("Branch004");
+    }
+}
